e774bd75a8150bbe84f7642ae1a4bc616882b70a
Alina <alina.bianca05@yahoo.com>
2024-04-09 11:11:48 +0300

    alina

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/*" element={<App/>} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
