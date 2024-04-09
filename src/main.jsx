31fa7c03893494f16a20d77fab928975178113e4
Alina <alina.bianca05@yahoo.com>
2024-04-09 11:22:39 +0300

    alina

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/DevMugsShop" element={<App/>} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>,
 )
